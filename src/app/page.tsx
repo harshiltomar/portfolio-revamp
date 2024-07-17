5648bdd6fefa1803343aeb97dcdc499bda7bf1e1
Harshil Tomar <harshiltomar20@gmail.com>
2024-07-17 13:09:24 +0530

    feat: navbar init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { ModeToggle } from "@/components/shared/theme-toggle";
+import { ModeToggle } from "@/components/micro/theme-toggle";
+import Navbar from "@/components/shared/Navbar";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HEll0
-      <ModeToggle/>
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      <Navbar/> 
     </main>
   );
 }
