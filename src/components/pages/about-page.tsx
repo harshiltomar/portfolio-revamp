00ea622041dba1997a65bccc25a130a69af7d267
Harshil Tomar <harshiltomar20@gmail.com>
2024-07-18 13:09:17 +0530

    fix: vercel apos issue

--- src/components/pages/about-page.tsx
+++ src/components/pages/about-page.tsx
@@ -7,7 +7,7 @@ export default function About() {
       //@ts-ignore
       <img className='rounded-3xl' src="https://user-images.githubusercontent.com/10498744/210012254-234538ff-d198-48aa-8964-37e6fd45d227.gif"/>
       <div className='mt-10'>
-        <text className='text-3xl font-serif'>Hello there! I'm Harshil Tomar ✨</text>
+        <text className='text-3xl font-serif'>Hello there! I&apos;m Harshil Tomar ✨</text>
         <ul className='mt-2'>
           <li className='text-zinc-500'>- I enjoy turning abstract ideas into tangible solutions through coding.</li>
           <li className='text-zinc-500'>- On a quest to expand my programming knowledge and skills in diverse tools and frameworks.</li>
