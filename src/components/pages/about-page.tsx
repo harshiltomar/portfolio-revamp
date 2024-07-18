3150a73d904368fb748f55ee7492f230fd970c13
Harshil Tomar <harshiltomar20@gmail.com>
2024-07-18 11:37:07 +0530

    feat: favicon change, experience init

--- src/components/pages/about-page.tsx
+++ src/components/pages/about-page.tsx
@@ -1,31 +1,20 @@
 import React from 'react'
+import Experience from '../micro/experience-list'
 
 export default function About() {
   return (
     <div>
-      <div>Hello there! I'm Harshil Tomar ✨
-          - I enjoy turning abstract ideas into tangible solutions through coding.
-          - On a quest to expand my programming knowledge and skills in diverse tools and frameworks.
+      <img className='rounded-3xl' src="https://user-images.githubusercontent.com/10498744/210012254-234538ff-d198-48aa-8964-37e6fd45d227.gif"/>
+      <div className='mt-4 text-zinc-700'>
+        <text className='text-3xl font-serif'>Hello there! I'm Harshil Tomar ✨</text>
+        <ul className='mt-2'>
+          <li>- I enjoy turning abstract ideas into tangible solutions through coding.</li>
+          <li>- On a quest to expand my programming knowledge and skills in diverse tools and frameworks.</li>
+        </ul>
+      </div>
 
-          Currently: Remote SWE Intern at HiFive Community
-          Previous
-          SDE Intern, NewGen Software
-          Fullstack Dev @Infiniti Solutions
-          Frontend Engineer @Spotline, USA (Remote)
-          UI/UX Designer, Fiverr
-          Computer Science and Engineering MSIT'24
-          I'm always open to collaboration and always excited about working with other developers on meaningful projects.
-
-          Connect w/m on LinkedIn @linkedIn/harshiltomar 📫
-          Connect w/m on Twitter @twitter/hartdrawss 📫
-          Check out my Resume @resume 📫
-
-          💻 Tech Stack
-          Languages: C++, JavaScript, Java
-          Backend Development: Express.js, Node.js, Backend Typescript
-          Frontend Development: HTML, CSS, React.js, Typescript, Next.js, Tailwind, Aceternity UI, Axios
-          Database: Firebase, MySQL, MongoDB, Prisma, PostGreSQL, GraphQL
-          Version Control: Git, GitHub, Docker
+      <div className='mt-5'>
+        <Experience/>
       </div>
     </div>
   )
