f4e3159f58aab90c5a692bb6d991ce78c890a828
Harshil Tomar <harshiltomar20@gmail.com>
2024-07-18 12:38:45 +0530

    feat: aceternity init

--- src/components/pages/about-page.tsx
+++ src/components/pages/about-page.tsx
@@ -6,7 +6,7 @@ export default function About() {
     <div>
       <img className='rounded-3xl' src="https://user-images.githubusercontent.com/10498744/210012254-234538ff-d198-48aa-8964-37e6fd45d227.gif"/>
       <div className='mt-4 text-zinc-700'>
-        <text className='text-3xl font-serif'>Hello there! I'm Harshil Tomar ✨</text>
+        <text className='text-3xl font-serif text-black'>Hello there! I'm Harshil Tomar ✨</text>
         <ul className='mt-2'>
           <li>- I enjoy turning abstract ideas into tangible solutions through coding.</li>
           <li>- On a quest to expand my programming knowledge and skills in diverse tools and frameworks.</li>
